f4a2f27e0e7d16c61643593e202553110b72881c
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2013-03-14 01:18:30 -0300

    improvements

--- puppet/manifests/mysql.pp
+++ puppet/manifests/mysql.pp
@@ -1,24 +1,10 @@
 class mysql::install {
-  package { "mysql-server":
-    ensure => present,
+  package {
+    'mysql-server': ensure => present;
   }
-
-  service { "mysql":
-    ensure => running,
-    require => Package["mysql-server"],
-  }
-
-  exec { "set-mysql-password":
-    unless  => "mysql -uroot -proot",
-    path    => ["/bin", "/usr/bin"],
-    command => "mysqladmin -uroot password root",
-    require => Service["mysql"],
-  }
-
-  exec { "create-database":
-    unless  => "/usr/bin/mysql -usite_development -psite_development site_development",
-    command => "/usr/bin/mysql -uroot -proot -e \"create database site_development; grant all on site_development.* to site_dev@localhost identified by 'site_development';\"",
-    require => Service["mysql"],
+  
+  service {
+    'mysql': ensure => running;
   }
 }
 
